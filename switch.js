d438f7b76ae98b34227cd4985badae285d6b6115
NavyaPalle <70169614+NavyaPalle@users.noreply.github.com>
2020-09-03 00:41:27 +0530

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,292 @@
+<!DOCTYPE>
+<head>
+<title>Switch Case</title>
+</head>
+<body>
+<script>
+var = 18
+switch (var == 18){
+case label '10'
+console.log(true)
+break;
+case label '11':
+console.log(true)
+break;
+case label '12':
+console.log(true)
+break;
+case label '13':
+console.log(true)
+break;
+case label '14':
+console.log(true)
+break;
+case label '15':
+console.log(true)
+break;
+case label '16'
+console.log(true)
+break;
+case label '17':
+console.log(true)
+break;
+case label '18':
+console.log(true)
+break;
+case label '19':
+console.log(true)
+break;
+case label '20':
+console.log(true)
+break;
+case label '21':
+console.log(true)
+break;
+case label '22'
+console.log(true)
+break;
+case label '23':
+console.log(true)
+break;
+case label '24':
+console.log(true)
+break;
+case label '25':
+console.log(true)
+break;
+case label '26':
+console.log(true)
+break;
+case label '27':
+console.log(true)
+break;
+case label '28'
+console.log(true)
+break;
+case label '29':
+console.log(true)
+break;
+case label '30':
+console.log(true)
+break;
+case label '31':
+console.log(true)
+break;
+case label '32':
+console.log(true)
+break;
+case label '33':
+console.log(true)
+break;
+case label '34'
+console.log(true)
+break;
+case label '35':
+console.log(true)
+break;
+case label '36':
+console.log(true)
+break;
+case label '37':
+console.log(true)
+break;
+case label '38':
+console.log(true)
+break;
+case label '39':
+console.log(true)
+break;
+case label '40'
+console.log(true)
+break;
+case label '41':
+console.log(true)
+break;
+case label '42':
+console.log(true)
+break;
+case label '43':
+console.log(true)
+break;
+case label '44':
+console.log(true)
+break;
+case label '45':
+console.log(true)
+break;
+case label '46'
+console.log(true)
+break;
+case label '47':
+console.log(true)
+break;
+case label '48':
+console.log(true)
+break;
+case label '49':
+console.log(true)
+break;
+case label '50':
+console.log(true)
+break;
+case label '51':
+console.log(true)
+break;
+case label '52'
+console.log(true)
+break;
+case label '53':
+console.log(true)
+break;
+case label '54':
+console.log(true)
+break;
+case label '55':
+console.log(true)
+break;
+case label '56':
+console.log(true)
+break;
+case label '57':
+console.log(true)
+break;
+case label '58'
+console.log(true)
+break;
+case label '59':
+console.log(true)
+break;
+case label '60':
+console.log(true)
+break;
+case label '61':
+console.log(true)
+break;
+case label '62':
+console.log(true)
+break;
+case label '63':
+console.log(true)
+break;
+case label '64'
+console.log(true)
+break;
+case label '65':
+console.log(true)
+break;
+case label '66':
+console.log(true)
+break;
+case label '67':
+console.log(true)
+break;
+case label '68':
+console.log(true)
+break;
+case label '69':
+console.log(true)
+break;
+case label '70'
+console.log(true)
+break;
+case label '71':
+console.log(true)
+break;
+case label '72':
+console.log(true)
+break;
+case label '73':
+console.log(true)
+break;
+case label '74':
+console.log(true)
+break;
+case label '75':
+console.log(true)
+break;
+case label '76'
+console.log(true)
+break;
+case label '77':
+console.log(true)
+break;
+case label '78':
+console.log(true)
+break;
+case label '79':
+console.log(true)
+break;
+case label '80':
+console.log(true)
+break;
+case label '81':
+console.log(true)
+break;
+case label '82'
+console.log(true)
+break;
+case label '83':
+console.log(true)
+break;
+case label '84':
+console.log(true)
+break;
+case label '85':
+console.log(true)
+break;
+case label '86':
+console.log(true)
+break;
+case label '87':
+console.log(true)
+break;
+case label '88'
+console.log(true)
+break;
+case label '89':
+console.log(true)
+break;
+case label '90':
+console.log(true)
+break;
+case label '91':
+console.log(true)
+break;
+case label '92':
+console.log(true)
+break;
+case label '93':
+console.log(true)
+break;
+case label '94'
+console.log(true)
+break;
+case label '95':
+console.log(true)
+break;
+case label '96':
+console.log(true)
+break;
+case label '97':
+console.log(true)
+break;
+case label '98':
+console.log(true)
+break;
+case label '99':
+console.log(true)
+break;
+case label '100'
+console.log(true)
+break;
+default:
+console.log(false)
+}
+
+
+
+
+
+
+
+
