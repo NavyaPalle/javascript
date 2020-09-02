7c6b579875ceb12605fba05a0a585f2856ad2ea0
NavyaPalle <70169614+NavyaPalle@users.noreply.github.com>
2020-09-02 13:19:05 +0530

    Create doc.js

---
+++ doc.js
@@ -0,0 +1,15 @@
+<!DOCTYPE>
+</html>
+<head>
+
+<title>DOC(Document Object model)</title>
+
+<script>
+var x = "DOC is a standard for how to get,change,add,or delete HTML<
+</script>
+
+</head>
+<body>
+</body>
+
+</html>
