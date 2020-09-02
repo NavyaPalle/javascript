428b2882d96288510bb2ec54c4f2469ece5925e7
NavyaPalle <70169614+NavyaPalle@users.noreply.github.com>
2020-09-03 01:50:05 +0530

    Update example.js

--- example.js
+++ example.js
@@ -1,3 +1,21 @@
-document.write("h1 style='background-color:yellow;color:blue'>Welcome to External Java Script</h1>");
-document.write("<br/>");
-document.write("Thank U ...!")
+<!DOCTYPE>
+  <html>
+  <head>
+  <title>Strings</title>
+  </head>
+<body>
+  <h2>JavaScript String Properties</h2>
+
+<p>The length property returns the length of a string:</p>
+
+<p id="demo"></p>
+
+<script>
+var txt = "Welcome to javascript";
+console.log(txt[5])
+console.log(txt[7])
+console.log(txt[10])
+document.getElementById("demo").innerHTML = txt.length;
+</script>
+</body>
+</html>
