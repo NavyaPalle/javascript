7972bab4b93958a994e7981040b725b6c7780c25
NavyaPalle <70169614+NavyaPalle@users.noreply.github.com>
2020-09-05 00:40:30 +0530

    Create toggle.js

---
+++ toggle.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html> 
+<html> 
+
+<head> 
+	<script src= 
+file:///C:/Users/DELL/Desktop/rdip/javascript.html> 
+	</script> 
+	<script> 
+		$(document).ready(function() { 
+			$("button").click(function() { 
+				$("#gfg").toggle(); 
+			}); 
+		}); 
+	</script> 
+	<style> 
+		#gfg { 
+			color: green; 
+			border: 5px solid black; 
+			width: 200px; 
+			text-align: center; 
+		} 
+	</style> 
+	</style> 
+</head> 
+
+<body> 
+
+	<div id="gfg">GeeksforGeeks</div> 
+
+	<button>Click to hide() and show() the above div</button> 
+
+</body> 
+
+</html> 
