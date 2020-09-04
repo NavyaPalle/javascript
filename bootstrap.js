0a4b1660a4cff3f6106cad83ba9a4eab3e992ab6
NavyaPalle <70169614+NavyaPalle@users.noreply.github.com>
2020-09-04 10:47:36 +0530

    Create bootstrap.js

---
+++ bootstrap.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+	
+<head> 
+	<title>Bootstrap Example</title> 
+
+	<meta charset="utf-8"> 
+
+	<meta name="viewport" content= 
+		"width=device-width, initial-scale=1"> 
+		
+	<!-- Bootstrap CSS library -->
+	<link rel="stylesheet" href= 
+"file:///C:/Users/DELL/Desktop/rdip/phoneno.html">
+	</head> 
+<body> 
+<h>Bootstrp Response for my webpage.</p>
+
+</body> 
+</html>					 
